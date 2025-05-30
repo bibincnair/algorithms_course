1ce7ff9a8d3bab222e70bd636524e200cea0e7df
bibin <bibincnair1@gmail.com>
2025-05-30 12:54:15 +0200

    add initial implementation and test cases for Game of Life algorithm

---
+++ pyalgos/src/pyalgos/game_of_life.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        pass
---
+++ pyalgos/tests/test_game_of_life.py
@@ -0,0 +1,10 @@
+import pytest
+from pyalgos.game_of_life import Solution
+
+
+def test_game_of_life_1():
+    pass
+
+def test_game_of_life_2():
+    pass
+
